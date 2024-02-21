829b5174aac99323ca88d00cfc86fdfdb169551d
Drayko <draykom@gmail.com>
2024-02-21 17:13:38 +1000

    Adding authentication button

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -5,11 +5,24 @@ import { ref } from 'vue'
 <template>
     <header>
         <h1>Reddit 2</h1>
+        <div id="auth">
+          <a href="https://www.reddit.com/api/v1/authorize?
+client_id=h4S3l2XkCCXeiqjERXO-5g
+&response_type=code
+&state=MY_STATE_STRING
+&redirect_uri=http://35.197.175.96/
+&duration=temporary
+&scope=identity">
+            <button>Authenticate</button>
+          </a>
+        </div>
     </header>
 </template>
 
 <style scoped>
 header {
-  margin-left: 2em;
+  margin: 0 2em;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
